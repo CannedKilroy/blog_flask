2855d8360333d1d39e76225ac7c912e0d4090d24
CannedKilroy <GenericArt@protonmail.com>
2024-08-09 17:14:04 -0600

    Removed registration

--- app/routes.py
+++ app/routes.py
@@ -90,20 +90,20 @@ def logout():
 """
 Register User
 """
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash(message='You are registered')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+#@app.route('/register', methods=['GET', 'POST'])
+#def register():
+#    if current_user.is_authenticated:
+#        return redirect(url_for('index'))
+#    form = RegistrationForm()
+#    
+#    if form.validate_on_submit():
+#        user = User(username=form.username.data, email=form.email.data)
+#        user.set_password(form.password.data)
+#        db.session.add(user)
+#        db.session.commit()
+#        flash(message='You are registered')
+#        return redirect(url_for('login'))
+#    return render_template('register.html', title='Register', form=form)
 
 
 """
